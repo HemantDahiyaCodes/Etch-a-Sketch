ffadfec3180f674a73491af1abfd9d5c629672bd
HemantDahiyaCodes <HemnatDahiyaCodes@gmail.com>
2024-09-25 20:27:18 +0530

    Completed the etch-a-sketch project

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const createGrid = () => {
+  const gridContainer = document.querySelector(".grid-container");
+  const size = +prompt("Enter the size of the grid");
+  if (size > 100) {
+    createGrid();
+    return;
+  }
+  // Creating rows
+  for (let i = 1; i <= size; i++) {
+    const gridRows = document.createElement("div");
+    gridRows.className = "row";
+    gridContainer.appendChild(gridRows);
+  }
+
+  // Creating columns
+  const rows = document.querySelectorAll(".row");
+  rows.forEach((row) => {
+    for (let i = 1; i <= size; i++) {
+      const gridCols = document.createElement("div");
+      gridCols.className = "column";
+      gridCols.style.flexBasis = `${100 / size}%`;
+      gridCols.addEventListener("mouseover", () => {
+        gridCols.style.backgroundColor = "blue";
+      })
+      row.appendChild(gridCols);
+    }
+  });
+};
+
+createGrid();
