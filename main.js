e1f975f3d20d98222a9c6b3d07c0a9e7e22db931
HemantDahiyaCodes <HemantDahiyaCodes@gmail.com>
2024-10-18 21:14:01 +0530

    Added random colors, keyboard functionality and some UI changes

--- main.js
+++ main.js
@@ -1,35 +1,75 @@
 "use strict";
+const getSize = document.getElementById("create-grid");
+const gridSize = document.getElementById("size");
 
 const createGrid = () => {
   const gridContainer = document.querySelector(".grid-container");
-  const size = +prompt("Enter the size of the grid");
-  if (size > 100) {
-    createGrid();
-    return;
+
+  // Clearing the exisitng grid
+  gridContainer.innerHTML = "";
+  // getting and setting the size of the grid.
+  function Size() {
+    const size = parseFloat(gridSize.value);
+
+    if (gridSize.value > 50) {
+      alert("Please enter a value less than 60!");
+      return;
+    }
+    
+    return size;
   }
+
   // Creating rows
-  for (let i = 1; i <= size; i++) {
+  for (let i = 1; i <= Size(); i++) {
     const gridRows = document.createElement("div");
     gridRows.className = "row";
     gridContainer.appendChild(gridRows);
   }
 
+  // Random Colors
+  // Red value
+  const red = () => {
+    const redColor = Math.floor(Math.random() * 255);
+    return redColor;
+  };
+
+  // Green value
+  const green = () => {
+    const greenColor = Math.floor(Math.random() * 255);
+    return greenColor;
+  };
+
+  // Blue value
+  const blue = () => {
+    const blueColor = Math.floor(Math.random() * 255);
+    return blueColor;
+  };
+
   // Creating columns
   const rows = document.querySelectorAll(".row");
   rows.forEach((row) => {
-    for (let i = 1; i <= size; i++) {
+    for (let i = 1; i <= Size(); i++) {
       const gridCols = document.createElement("div");
       gridCols.className = "column";
-      gridCols.style.flexBasis = `${100 / size}%`;
+      gridCols.style.flexBasis = `${100 / Size()}%`;
       gridCols.addEventListener("mouseover", () => {
-        gridCols.style.backgroundColor = "blue";
-      })
+        gridCols.style.backgroundColor = `rgb(${red()}, ${green()}, ${blue()})`;
+      });
       gridCols.addEventListener("click", () => {
-        gridCols.style.backgroundColor = "blue";
-      })
+        gridCols.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+      });
       row.appendChild(gridCols);
     }
   });
 };
 
-createGrid();
+getSize.addEventListener("click", createGrid);
+
+// making the program work by hitting the enter on keyboard
+const handleClick = (event) => {
+  if(event.key === "Enter") {
+    createGrid();
+  }
+}
+
+gridSize.addEventListener("keydown", handleClick)
